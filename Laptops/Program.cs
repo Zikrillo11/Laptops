ef4572b55351e8b6513e0cb71ca72c2ea30b1774
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-05-21 14:36:13 +0500

    oken migration qildi

--- Laptops/Program.cs
+++ Laptops/Program.cs
@@ -1,9 +1,17 @@
+using Laptops.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
