85319a5ec26e9f0a9f87c72584b7ffd54a42d38a
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-05-21 13:50:19 +0500

    modellar yozildi

---
+++ Laptops.Domain/Entites/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laptops.Domain.Entites;
+
+[Table("Category")]
+public class Category : BaceEntity
+{
+    [Column("Name")]
+    public string Name { get; set; }
+
+    public ICollection<Laptop> Laptops { get; set; } = new List<Laptop>();
+}
---
+++ Laptops.Domain/Entites/Laptop.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laptops.Domain.Entites;
+
+
+[Table("Laptop")]
+public class Laptop : BaceEntity
+{
+    [Column("Nmae")]
+    public string Name { get; set; }
+
+    [Column("Brand")]
+    public string Brand { get; set; }
+
+    [Column("Processor")]
+    public string Processor { get; set; }
+
+    [Column("Ram")]
+    public int Ram { get; set; }
+
+    [Column("Storage")]
+    public int Storage { get; set; }
+
+    [Column("StorageType")]
+    public string StorageType { get; set; }
+
+    [Column("Price")]
+    public decimal Price { get; set; }
+
+    [Column("ScreenSize")]
+    public double ScreenSize { get; set; }
+
+    [Column("GPU")]
+    public string GPU { get; set; }
+
+    [Column("OperatingSystem")]
+    public string OperatingSystem { get; set; }
+
+    [Column("BatteryLife")]
+    public int BatteryLife { get; set; }
+
+    [Column("IsTouchScreen")]
+    public bool IsTouchScreen { get; set; }
+
+    [Column("Color")]
+    public string Color { get; set; }
+
+    [Column("Stock")]
+    public int Stock { get; set; }
+
+    [Column("CategoryId")]
+    public Guid CategoryId { get; set; }
+
+    [Column("Category")]
+    public Category Category { get; set; }
+
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    public ICollection<LaptopImage> Images { get; set; } = new List<LaptopImage>();
+}
