ed1d7d83e332b54c8a5c4dbdac54caa511091428
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-05-21 14:38:50 +0500

    f

--- Laptops/Controllers/HomeController.cs
+++ Laptops/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Laptops.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(); 
         }
         public IActionResult Error()
         {
